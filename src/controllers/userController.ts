f379eb11b74b77b96b197589f32800019983b751
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2025-03-02 13:11:33 +0530

    Add signup signin route

---
+++ src/controllers/userController.ts
@@ -0,0 +1,82 @@
+import asyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+import prisma from "../lib/prisma";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+// @ts-ignore
+export const registerUser = asyncHandler(async (req: Request, res: Response) => {
+    const {username} = req.body;
+    if (!username) {
+        return res.status(400).json({message: "Username is required"});
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            username: username,
+        },
+    })
+
+    if (user) {
+        return res.status(400).json({message: "Username already exists"});
+    }
+
+    const userCreated = await prisma.user.create({
+        data: {
+            username: username,
+        },
+    });
+
+    const exp = Date.now() + 1000 * 60 * 60 * 5;
+    const token = jwt.sign({ sub: userCreated.id, exp }, process.env.SECRET!);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: userCreated.id,
+        username: userCreated.username,
+      },
+    });
+});
+
+// @ts-ignore
+export const loginUser = asyncHandler(async (req: Request, res: Response) => {
+    const {username} = req.body;
+
+    const user = await prisma.user.findFirst({
+        where: {
+            username: username,
+        },
+    });
+
+    if (!user) {
+        return res.status(404).json({message: "User not found"});
+    }
+
+    const exp = Date.now() + 1000 * 60 * 60 * 5;
+    const token = jwt.sign({ sub: user.id, exp }, process.env.SECRET!);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+    });
+
+    res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        id: user.id,
+        username: user.username,
+      },
+    });
+});
+
+export const signout = asyncHandler(async(req: Request, res: Response) => {
+    res.clearCookie("token", { path: "/", httpOnly: true, secure: true });
+
+    res.json({ message: "Signed out successfully" });
+})
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,4 +1,10 @@
 import express from "express";
 import requireAuth from "../middleware/auth";
+import { loginUser, registerUser, signout } from "../controllers/userController";
 const router = express.Router();
 
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/signout').get(signout);
+
+export default router;
